31deacdeb98cc03447d051285a885bf4d6a21871
CalumRaine <calumsicoding@gmail.com>
2021-05-07 16:19:51 +0100

    cleaned up code

--- ncurses_test.c
+++ ncurses_test.c
@@ -1,10 +1,6 @@
-#include <ncurses.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <ncurses.h>
 #include <time.h>
-
-enum boolean {FALSE=0, TRUE=1};
 
 struct snakePos{
 	int x;	
@@ -12,19 +8,19 @@ struct snakePos{
 };
 
 int main(){	
-	srand(time(NULL));
-	initscr();			// Start curses mode 		  
+	srand(time(NULL));		// initialise random number generator
+	initscr();			// start curses mode 		  
 	cbreak();			// make input characters immediately available (no buffering)
-	nodelay(stdscr,1);
+	nodelay(stdscr,1);		// do not stall and wait for user input
 	noecho();			// do not print input characters to the screen
 	keypad(stdscr,1);		// make the arrow keys work
-	curs_set(FALSE);
-	attron(A_BOLD);
+	curs_set(FALSE);		// invisible cursor
+	attron(A_BOLD);			// make all characters bold
 	
 	// INITIALISE COLOUR
 	start_color();
-	init_pair(6,COLOR_YELLOW,COLOR_RED);
-	init_pair(1,COLOR_GREEN,COLOR_GREEN);
+	init_pair(6,COLOR_YELLOW,COLOR_RED);		// this one is used for title bar and "Game Over" snake
+	init_pair(1,COLOR_GREEN,COLOR_GREEN);		// others used as food/snake colours
 	init_pair(2,COLOR_BLUE,COLOR_BLUE);
 	init_pair(3,COLOR_MAGENTA,COLOR_MAGENTA);
 	init_pair(4,COLOR_CYAN,COLOR_CYAN);
@@ -50,7 +46,7 @@ int main(){
 	food.y=(rand()%(maxY-1))+1;
 	mvaddch(food.y,food.x,' ' | COLOR_PAIR(foodColour));
 
-	// INITIALISE SCORES
+	// INITIALISE TITLE BAR
 	int foodCounter=0, level=1;
 	move(0,0);
 	attron(COLOR_PAIR(6));
@@ -64,58 +60,71 @@ int main(){
 	while ( (latestArrow=getch()) == ERR );
 
 	for (int yDirection=0, xDirection=1, nextArrow=ERR, delay=10000000; 1 ; nextArrow=getch()){
-		if (nextArrow!=ERR) latestArrow=nextArrow;
+		if (nextArrow!=ERR) latestArrow=nextArrow; 						// update input only when user presses key
 		if (latestArrow == KEY_UP && xDirection) yDirection=-1, xDirection=0;
 		else if (latestArrow == KEY_DOWN && xDirection) yDirection=1, xDirection=0;
 		else if (latestArrow == KEY_RIGHT && yDirection) yDirection=0, xDirection=1;
 		else if (latestArrow == KEY_LEFT && yDirection) yDirection=0, xDirection=-1;
 		else if (latestArrow == 'c') goto end;
 
+		// FIND NEXT POSITION
 		int newX=snakeStack[headPos].x + xDirection;
 		int newY=snakeStack[headPos].y + yDirection;
+
+		// CHECK SCREEN BOUNDARIES
 		if (newX<0) newX=maxX;
 		else if (newX==maxX) newX=0;
 		if (newY==0) newY=maxY;
 		else if (newY==maxY) newY=1;
+
+		// CHECK IF EATEN FOOD
 		if (newX==food.x && newY==food.y){
-			for (int segment=snakeLength; segment>headPos+1; --segment) snakeStack[segment]=snakeStack[segment-1];
-			++headPos;
+			// add new entry to snake stack
+			for (int segment=snakeLength; segment>headPos+1; --segment) snakeStack[segment]=snakeStack[segment-1]; 
+			++snakeLength;
+
+			// add new segment to snake
+			++headPos;			
 			snakeStack[headPos].x=food.x;
 			snakeStack[headPos].y=food.y;
-			++snakeLength;
+
+			// generate new food position
 			food.x=rand()%maxX;
 			food.y=(rand()%(maxY-1))+1;
-			snakeColour=foodColour;
-			foodColour=(foodColour%5)+1;
-			mvaddch(food.y,food.x,' ' | COLOR_PAIR(foodColour));
-			if ( (++foodCounter%5)==0 ) {
-				delay-=1000000;
-				++level;
-			}
+			snakeColour=foodColour;					// snake adopts food colour
+			foodColour=(foodColour%5)+1;				// new food colour
+			mvaddch(food.y,food.x,' ' | COLOR_PAIR(foodColour)); 	// draw new food
+
+			// check if level needs to increase
+			if ( (++foodCounter%5)==0 ) delay-=1000000, ++level;	// make faster
+
+			// update title bar with new score
 			move(0,0);
 			attron(COLOR_PAIR(6));
 			for (int i=0; i<maxX; ++i) addch(' ');
 			mvprintw(0,0,"SNAKE\tLevel %i\tScore %i",level,foodCounter);
 			attroff(COLOR_PAIR(6));
 		}
-		else {
-			headPos=(headPos+1)%snakeLength;
+		else { // OTHERWISE CLEAR TAIL POS AND ADD NEW HEAD POS
+			headPos=(headPos+1)%snakeLength; 
 			mvaddch(snakeStack[headPos].y,snakeStack[headPos].x,' ' | COLOR_PAIR(0)); 
 			snakeStack[headPos].x=newX;
 			snakeStack[headPos].y=newY;
 			mvaddch(snakeStack[headPos].y,snakeStack[headPos].x,' ' | COLOR_PAIR(snakeColour));
+			// CHECK FOR COLLISION
 			for (int segment=0, check=(headPos+1)%snakeLength; segment<snakeLength-1; ++segment, check=(check+1)%snakeLength){
 				if (snakeStack[check].x==snakeStack[headPos].x && snakeStack[check].y==snakeStack[headPos].y) goto end;
 			}
 		}
 		refresh();
-		for (int i=0; i<delay; ++i);
+		for (int i=0; i<delay; ++i); // this should be updated to a true delay function, otherwise very processor-dependent
 	}
 
+	// COLLISION DETECTED: paint snake red and pause
 	end:
 	for (int segment=0; segment<snakeLength; ++segment) mvaddch(snakeStack[segment].y,snakeStack[segment].x,' ' | COLOR_PAIR(6));
 	refresh();
-	while(1);
+	while(1);			// bad practice, program never finishes
 	endwin();			// End curses mode		  
 	return 0;
 }
